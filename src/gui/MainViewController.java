749ce5cd830c333fb257c4d128419bdb9eefa0e8
Daniel lucas <Daniel@DK-ADM1.MLIBANIO.LOCAL>
2019-09-30 16:16:12 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 	
@@ -29,7 +38,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
@@ -37,5 +46,43 @@ public class MainViewController implements Initializable {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	//abre outra tela
+	private synchronized void loadView(String absoluteName) {
+		
+		try {
+			//carregar outra tela [/gui/About.fxml]
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			//carrega uma nova VBox
+			VBox newVBox = loader.load();
+			
+			//referencia da cena para trabalhar com a janela principal
+			Scene mainScene = Main.getMainScene();
+			//referencia VBox da janela principal
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); //pega o primeiro elemento da view (ScrollPane)
+			//getContent já é uma referencia ao que existe dentro da scrollPane [VBox]
+			
+			
+			/*   ACRESCENTA NOS FILHOS DO VBOX OS FILHOS DO VBOX DA JANELA ABOUT   
+			 * PRESERVA O MENUBAR
+			 * EXCLUI OS FILHOS DO VBOX
+			 * INCLUIR O MENUBAR
+			 * INCLUIR OS FILHOS DO VBOX DA JANELA ABOUT*/
+			
+			//referencia para o menu (primeiro filho do VBox da janela principal)
+			Node mainMenu = mainVBox.getChildren().get(0);
+			//limpar todos os filhos de mainVBox
+			mainVBox.getChildren().clear();
+			//add filhos de mainMenu
+			mainVBox.getChildren().add(mainMenu);
+			//add filhos de newVBox [Coleçao]
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			/* MANIPULA A CENA PRINCIPAL INCLUINDO NELA ALEM DO MENU PRINCIPAL, OS FILHOS DA JANELA QUE ESTIVER ABRINDO */
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
